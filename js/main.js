dd994688c789f97f503859c4cbf32ff846fed574
CamilaCosentino <camucosen@gmail.com>
2023-07-20 12:16:13 -0300

    upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector(".modal");
+const trigger = document.querySelectorAll(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+toggleModal = () => {
+  modal.classList.toggle("show__modal");
+};
+
+windowOnClick = (event) => {
+  if (event.target === modal) {
+    toggleModal();
+  }
+};
+trigger.forEach((btn) => {
+  btn.addEventListener("click", toggleModal);
+});
+// Close the widget with the esc key
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && modal.classList.contains("show__modal")) {
+    toggleModal();
+  }
+});
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+// Sending Email with SMPJS

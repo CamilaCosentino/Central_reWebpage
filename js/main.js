41a1d4eb2cfc1167c34db3024bf4c8e24b289ecb
CamilaCosentino <camucosen@gmail.com>
2023-07-24 13:50:56 -0300

    CSS update

--- js/main.js
+++ js/main.js
@@ -23,28 +23,3 @@ window.addEventListener("keydown", (e) => {
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-// Sending Email with SMPJS
-
-submit.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    sendForm();
-  });
-});
-
-function sendForm() {
-  Email.send({
-    SecureToken: "88e974a3-cc83-4dac-a4eb-6223f95c12be",
-    To: "marketing@auladig.co",
-    From: "alejo921017@gmail.com",
-    Subject: "New contact appointment",
-    Body: `Este es el cuerpo del mensaje`,
-  }).then((message) => {
-    if (message === "OK") {
-      console.log("message:", message);
-      console.log("entre a OK");
-    } else {
-      console.log("Error");
-    }
-  });
-}
